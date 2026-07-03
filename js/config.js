f64f7a6391c841c7ac31bbe7344813fb2179d9f0
safaYa12 <suhailariyallur1@gmail.com>
2026-07-03 13:48:32 +0530

    Change Cognito user pool IDs in config.js
    Updated AWS Cognito user pool configuration.

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_PoGMQf5RA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6m2l4sollhr882llb6oi9c394n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_hhT1kddBP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7q9om9u80db8q6t547mmmvjfrs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
